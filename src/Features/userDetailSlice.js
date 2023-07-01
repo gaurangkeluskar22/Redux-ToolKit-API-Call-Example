0db0c3d638573b9a197219b96d1525769c404576
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-07-01 16:24:08 +0530

    created navbar

---
+++ src/Features/userDetailSlice.js
@@ -0,0 +1,12 @@
+import {createSlice } from '@reduxjs/toolkit';
+
+export const userDetailSlice = createSlice({
+    name : "userDetail",
+    initialState : {
+        users : [],
+        loading : false,
+        error : null,
+    },
+});
+
+export default userDetail.reducer;
--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
 import { configureStore} from '@reduxjs/toolkit'
+import userDetail from './Features/userDetailSlice'
 
 export const store = configureStore({
     reducer:{
-
+        app : userDetail,
     }
 });
+
+
+
 
