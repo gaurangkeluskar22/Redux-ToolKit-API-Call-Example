6a61b6672f406dc5e880470dc33d2573bde96f19
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-07-01 17:33:45 +0530

    added code to create user

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
-import './App.css';
-import Navbar from './Components/Navbar';
+import "./App.css";
+import Create from "./Components/Create";
+import Navbar from "./Components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Create />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Create.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+const Create = () => {
+  const [users, setUsers] = useState({});
+
+  const getUsersData = (e) => {
+    setUsers({ ...users, [e.target.name]: e.target.value });
+    console.log(users);
+  };
+
+  return (
+    <form className="w-50 m-5 mx-auto">
+      <div className="form-group my-1">
+        <label for="exampleInputEmail1">Name</label>
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          placeholder="Enter Name"
+          onChange={getUsersData}
+        />
+      </div>
+      <div className="form-group my-1">
+        <label for="exampleInputEmail1">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter Email"
+          name="email"
+          onChange={getUsersData}
+        />
+      </div>
+      <div className="form-group my-1">
+        <label for="exampleInputEmail1">Age</label>
+        <input
+          type="number"
+          name="age"
+          className="form-control"
+          placeholder="Enter Age"
+          onChange={getUsersData}
+        />
+      </div>
+      <div className="w-5">
+        <div className="mb-3">
+          <input
+            className="form-check-input"
+            name="gender"
+            value="Male"
+            type="radio"
+            onChange={getUsersData}
+          />
+          <label className="form-check-label">Male</label>
+        </div>
+        <div className="mb-3">
+          <input
+            className="form-check-input"
+            name="gender"
+            value="Female"
+            type="radio"
+            onChange={getUsersData}
+          />
+          <label className="form-check-label">Female</label>
+        </div>
+      </div>
+      <button type="submit" className="btn btn-primary my-1">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Create;
