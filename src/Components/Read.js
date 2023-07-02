7bddad5055ca66a87cb771136658588deed7e5fe
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2023-07-02 18:47:19 +0530

    added edit and delete button

--- src/Components/Read.js
+++ src/Components/Read.js
@@ -16,14 +16,33 @@ const Read = () => {
 
   return (
     <>
-      <h2>All Data</h2>
+      <h2>All Users Data</h2>
       {console.log(users)}
       {users && users.map((ele) => {
         return(
-        <div className="card w-50 mx-auto" key={ele.key}>
+        <div className="card w-50 mx-auto m-2" key={ele.id}>
           <div className="card-body">
-            <h5 className="card-title">{ele.name}</h5>
-            <p className="card-text">{ele.email}</p>
+            <h5 className="card-title">Name : {ele.name}</h5>
+            <p className="card-text">Email ID : {ele.email}</p>
+            <div className="row">
+                <div className="col-lg-6">
+                <p className="card-text">Gender : {ele.gender}</p>
+                </div>
+                <div className="col-lg-6">
+                <p className="card-text">Age : {ele.age}</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-6">
+                <button className="card-text btn btn-primary">Edit</button>
+                </div>
+                <div className="col-lg-6">
+                <button className="card-text btn btn-danger">Delete</button>
+                </div>
+            </div>
+            
+            
+            
           </div>
         </div>
         );
